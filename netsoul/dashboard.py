609e9baf0bef4512acb22c25ea66fc2e0468670f
Emmanuel Solomo <emmanuel@webfutur.com>
2019-05-30 15:09:36 +0100

    update

---
+++ netsoul/dashboard.py
@@ -0,0 +1,88 @@
+import datetime
+import dateutil.parser
+import pandas as pd
+from datetime import timedelta
+from .models import NsLog, O365User
+from .serializers import NsLogSerializer, O365UserSerializer
+import pytz
+
+
+
+class DashBoardManager(object):
+    """Log and Activity Manager """
+    def __init__(self, email, ip=None):
+        self.UserActivity = O365User.objects.get(email=email)
+        self.timezone = pytz.timezone("Africa/Porto-Novo")
+        self.now = datetime.datetime.now()
+        self.logtime = self.timezone.localize(self.now)
+        self.TzStrLogtime = self.logtime.strftime('%Y-%m-%dT%H:%M:%S%z')
+        self.StrLogtime = self.logtime.strftime('%Y-%m-%dT%H:%M:%S')
+        self.today = datetime.date.today()
+        self.day = datetime.date.today()
+        self.fullDay = self.day.strftime('%Y-%m-%dT%H:%M:%S')
+        self.date = datetime.date.today().strftime('%Y-%m-%dT%H:%M:%S%z')
+        self.simpleDate=datetime.date.today().strftime('%Y-%m-%d')
+        self.nslogs = NsLog.objects.filter(user=email,date=self.day)
+        self.nbLog = len(self.nslogs)
+        self.serializer = NsLogSerializer(self.nslogs, many=True)
+        self.TzStart=str(self.day) + '+01:00'
+        self.start=str(self.day)
+        self.TzfullDayStart=str(self.fullDay) + '+01:00'
+        self.end=self.serializer.data[0]['timestamp']
+        self.ip = ip
+        self.email = email
+        self.setNoActivityLogs()
+        self.find_inactivy()
+        self.timeShift = self.getTimeDiff()
+        self.dashboardData = self.noActivityLogs +  self.dataUpdate
+
+    def setNoActivityLogs(self):
+        stamps = pd.date_range(start=self.TzfullDayStart, end=self.end,freq='0H10T')
+        serie = pd.Series('timestamp', index=stamps.strftime('%Y-%m-%dT%H:%M:%S%z'))
+        data = dict(zip(serie.index.format(), serie))
+        self.noActivityLogs = [ dict(user=self.email,date=self.simpleDate,timestamp=self.date,last_signin=None,ip=None,active=False)  for  date, key in  data.items()]
+
+    def findLogOffTime(self):
+        stamps = pd.date_range(start=self.TzfullDayStart, end=self.end,freq='0H10T')
+        serie = pd.Series('timestamp', index=stamps.strftime('%Y-%m-%dT%H:%M:%S%z'))
+        data = dict(zip(serie.index.format(), serie))
+        self.noActivityLogs = [ dict(user=self.email,date=self.simpleDate,timestamp=self.date,last_signin=None,ip=None,active=False)  for  date, key in  data.items()]
+
+    def getTimeDiff(self):
+        if self.nbLog > 0:
+            time_difference = self.logtime - self.nslogs[self.nbLog - 1].timestamp
+            time_difference_in_minutes = time_difference / timedelta(minutes=1)
+            return time_difference_in_minutes
+        return 0
+
+    def check_timedelta(self, time1, time2):  
+        date_time2_obj = datetime.datetime.strptime(time2.replace("+01:00", ""), '%Y-%m-%dT%H:%M:%S')
+        date_time1_obj = datetime.datetime.strptime(time1.replace("+01:00", ""), '%Y-%m-%dT%H:%M:%S')  
+        time_difference =  date_time1_obj - date_time2_obj
+
+        time_difference_in_minutes = time_difference / timedelta(minutes=1)
+        shift_array = []
+        if (time_difference_in_minutes > 15):    
+            shift = 1
+            new_row = date_time2_obj
+            while shift < (time_difference_in_minutes / 5):      
+                new_row = new_row + timedelta(minutes=5)
+                log = dict(user=self.email,date=self.day,timestamp=new_row.strftime('%Y-%m-%dT%H:%M:%S%z'),last_signin=None,ip=None,active=False)
+                shift_array.append(log)
+                shift = shift + 1
+        return shift_array
+
+    def find_inactivy(self):
+        current = 0
+        self.dataUpdate = self.serializer.data
+        while current < len(self.dataUpdate):
+            if current > 0:
+                shift_array = self.check_timedelta(self.dataUpdate [current]['timestamp'], self.dataUpdate [current - 1]['timestamp'])
+                if (len(shift_array) > 0):
+                    cpt = 0 
+                    index = current
+                    while(cpt < len(shift_array)):
+                        self.dataUpdate.insert(index, shift_array[cpt])
+                        cpt = cpt + 1
+                        index = index + 1          
+            current += 1
---
+++ netsoul/logs.py
@@ -0,0 +1,58 @@
+import datetime
+import dateutil.parser
+import pandas as pd
+from datetime import timedelta
+from .models import NsLog, O365User
+from .serializers import NsLogSerializer, O365UserSerializer
+import pytz
+
+
+
+class LogManager(object):
+    """Log and Activity Manager """
+    def __init__(self, email, ip=None):
+        self.UserActivity = O365User.objects.get(email=email)
+        self.timezone = pytz.timezone("Africa/Porto-Novo")
+        self.today = datetime.date.today()
+        self.now = datetime.datetime.now()
+        self.nslogs = NsLog.objects.filter(user=email,date=self.today)
+        self.nbLog = len(self.nslogs)
+        self.logtime = self.timezone.localize(self.now)
+        self.TzStrLogtime = self.logtime.strftime('%Y-%m-%dT%H:%M:%S%z')
+        self.StrLogtime = self.logtime.strftime('%Y-%m-%dT%H:%M:%S')
+        self.timeShift = self.getTimeDiff()
+        self.email = email
+        self.ip = ip
+
+    def updateActivity(self, last_signin=None):
+        self.UserActivity.active = True
+        self.UserActivity.last_activity = self.logtime
+        self.UserActivity.last_seen = str(round(self.timeShift)) + " minutes ago"
+        if self.ip == "137.255.10.29" or self.ip == "41.85.161.132":
+            self.UserActivity.location = "[EPITECHxSemeCity][2nd Floor] HEAP"
+        elif self.ip == "137.255.8.213":
+            self.UserActivity.location = "[EPITECHxSemeCity][Ground Floor] ADM"
+        else:
+            self.UserActivity.location = "[SomewherexWorld]"
+        self.UserActivity.save()
+
+    def updateNsLog(self,last_signin='None', originWeb=True):
+        data = dict(user=self.email,timestamp=self.TzStrLogtime, ip=self.ip,active=True)
+        print(data)
+        print(self.timeShift)
+        serializer = NsLogSerializer(data=data)
+        if serializer.is_valid() and self.timeShift >= 5 and originWeb:
+            serializer.save()
+        elif serializer.is_valid() and not originWeb :
+            serializer.save()   
+        else :
+            print("Error Saving logs")
+            print(serializer.errors)
+
+    def getTimeDiff(self):
+        if self.nbLog > 0:
+            time_difference = self.logtime - self.nslogs[self.nbLog - 1].timestamp
+            time_difference_in_minutes = time_difference / timedelta(minutes=1)
+            return time_difference_in_minutes
+        return 0
+
